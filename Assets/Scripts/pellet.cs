95a854ba0fe9acd10b58a124418c5857b2c4815e
Xevans <43121988+Xevans@users.noreply.github.com>
2024-07-30 16:12:50 -0400

    basic player movement added

---
+++ Assets/Scripts/pellet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class pellet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name + " has collided with the pellet");
+    }
+}
---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField]private float speed;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+    }
+}
